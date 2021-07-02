9bda5a6729ac486406ad83a311863da264a84882
Hallie <hine113@gmail.com>
2021-07-02 09:49:20 -0500

    modifying projects

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {goals} from '../utils/performance-goals';
 import Card from '../components/Card';
-import image from '../images/photo.jpg';
 function AboutPage(){
 	return(
 		<div className="AboutPage" id="About">
--- src/pages/ProjectPage.js
+++ src/pages/ProjectPage.js
@@ -1,16 +1,34 @@
 import React from 'react';
-
+import frontImage from '../images/twist-front.jpg';
+import image from '../images/twist.jpg';
 function ProjectPage(){
+	const toggleProjectDetails = e => {
+		let projectDetails = document.getElementById("TwistDetails");
+		if (projectDetails.style.width == '0px'){
+			projectDetails.style.width='100%';
+			projectDetails.style.height='100%';
+			projectDetails.style.zIndex = '1';
+
+		} 
+		else {
+			projectDetails.style.width = 0;
+			projectDetails.style.height = 0;
+			projectDetails.style.zIndex = 0;
+		}
+	}
 	return(
 		<div className="ProjectPage" id="Project">
 				<h1>Projects</h1>
 				<div className="projects">
-				    <iframe className="project" src="https://codepen.io/minitrash/full/dmYPYj/"></iframe>
-				    <iframe className="project" src="https://codepen.io/minitrash/full/rJgxBV/"></iframe>
-				    <iframe className="project" src="https://codepen.io/minitrash/full/GxNoaQ/"></iframe>
-				    <iframe className="project" src="https://codepen.io/minitrash/full/MQLMRy/"></iframe>
-				    <iframe className="project" src="https://codepen.io/minitrash/full/LQoyxX/"></iframe>
-				    <iframe className="project" src="https://codepen.io/minitrash/full/yKBBNB/"></iframe>
+					<div className="Twist" onClick={toggleProjectDetails}>
+						<img src={frontImage} alt="Twist"/>
+						<h2>Twist</h2>
+						<p>ReactJS | Scss | Node.js</p>
+						<p>A web app allowing users to store their personal recipes while also exploring and modifying others' recipes</p>
+					</div>
+					<div id="TwistDetails"  onClick={toggleProjectDetails}>
+						<img src={image} alt="Twist"/>
+					</div>
   				</div>
 		</div>
 	);
